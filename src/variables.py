6b6017d23f9d3085e3b8ca40ecf194eef0d6c892
Neus Pou Amengual <neuspouamengual@gmail.com>
2024-08-07 11:59:50 +0200

    variables file

---
+++ src/variables.py
@@ -0,0 +1,14 @@
+
+GROUP = "group"
+SAMPLE = "sample"
+INCHIKEY = 'inchikey'
+MOLID = 'metabolite_id'
+MOLNM = "metabolite_name"
+ALIGNID = 'alignid'
+PATHID = 'pathway_id'
+PATHNM = 'pathway_name'
+PVALUE = 'p-Value'
+PATH_COM = "Pathway Compounds"
+HITS = "Hits"
+PATH_COV = "Pathway Coverage (%)"
+PATH_SIG = "Pathway Significance"
