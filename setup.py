9dbc4b1fb4f888850dd76c7e41bc409950c4b5a4
Neus Pou Amengual <neuspouamengual@gmail.com>
2024-10-21 15:08:24 +0200

    v1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ REQUIREMENTS = ['scipy', 'numpy','pandas', 'scikit-learn']
 
 setup(
     name='oloPATH',
-    version='1.1',
+    version='1.2',
     description='Package for metabolomic pathways analysis',
     author='Neus Pou Amengual',
     author_email='np@olobion.ai',
